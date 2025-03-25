4c63a812fdd0ef95503e126916800027c44200b7
JGMink <jonah@planetminkoff.com>
2025-03-25 15:41:01 -0400

    Adapted Files for Mac Implementation
     - Improved Docker files in hopes of later readaptation
     - Altered app.py to render the tables on run and specify local run port
     - Added Requirements for mac specific as requirements_mac.txt and specific windows requirements page through rename

--- Sprint 1/AI Fitness Project/app.py
+++ Sprint 1/AI Fitness Project/app.py
@@ -84,4 +84,6 @@ def register():
 
  
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()  
+    app.run(debug=True, host="0.0.0.0", port=5000)
