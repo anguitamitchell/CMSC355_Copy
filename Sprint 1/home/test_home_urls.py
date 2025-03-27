3dba3734bb95829bc0d5390389a5ccef54a4f9cf
MattRoss05 <rossmj3@vcu.edu>
2025-03-27 18:14:39 -0400

    Delete Sprint 1/home directory

--- Sprint 1/home/test_home_urls.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-# Create your tests here.
-class HomeUrlTest(TestCase):
-    def testHome(self):
-        response = self.client.get(reverse('welcome'))
-        self.assertEqual(response.status_code, 200)
--- Sprint 1/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-#Render welcome page
-def welcome_page(request):
-    return render(request, 'home/welcome.html')
